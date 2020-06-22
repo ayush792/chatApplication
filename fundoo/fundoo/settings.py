a7acf776801b815b65b326bf902d2078db187f1c
ayush792 <63246727+ayush792@users.noreply.github.com>
2020-06-22 22:03:15 +0530

    host updated

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -106,7 +106,7 @@ DATABASES = {
         'NAME': 'DatabaseChatApp',
         'USER': 'chatAppDatabase',
         'PASSWORD': 'Ayushverma123',
-        'HOST': 'terraform-20200622120903123300000002.ckyvieifwbgr.us-east-2.rds.amazonaws.com',
+        'HOST': 'terraform-20200622161202587300000002.ckyvieifwbgr.us-east-2.rds.amazonaws.com',
         'PORT': '5432',
     }
 }
