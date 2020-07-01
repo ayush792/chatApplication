83617c239b8199d4aba88c02c465750f90e23666
Ayush Verma <63246727+ayush792@users.noreply.github.com>
2020-07-01 19:36:32 +0530

    updated settings.py

--- fundoo/fundoo/settings.py
+++ fundoo/fundoo/settings.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = ['*']
 EMAIL_USE_TLS = True            # Email Tools true
 EMAIL_HOST = 'smtp.gmail.com'   # SMTP protocol for mail transfer
 EMAIL_HOST_USER = 'vayush198@gmail.com'   # email to be send from user
-EMAIL_HOST_PASSWORD = 'Ayush123@'                   # password
+EMAIL_HOST_PASSWORD = 'Ayush1234@'                   # password
 EMAIL_PORT = 587                                       # default email port
 DEFAULT_FROM_EMAIL = 'TestSite Team <noreply@example.com>'  # subject for email
 
